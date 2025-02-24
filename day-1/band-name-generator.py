63b94316992c3b0b3450fc191b151490d4cf8363
Eko <2010031802036@sar.ac.id>
2025-02-24 12:01:47 +0700

    day-1 project : band-name-generator

---
+++ day-1/band-name-generator.py
@@ -0,0 +1,14 @@
+# Band Name Generator Project
+# This program can generate a band name based on the city and pet name of the user.
+
+# Welcome to massage
+print("Welcome to the Band Name Generator.")
+
+# Get the city's name form user
+city = input("What's the name of the city you grew up in?\n")
+
+# Get name pet form user
+name_of_the_pet = input("What's your pet's name?\n")
+
+# Print Band Name
+print("Your band name could be " + city + " " + name_of_the_pet)
