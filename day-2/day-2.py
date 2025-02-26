cf0e62b4faa74aee0700e55fa72c6543b6906d9c
ulung3ko <akunke2eko@gmail.com>
2025-02-26 12:20:24 +0700

    Day-2 : Task type conversion has finished

--- day-2/day-2.py
+++ day-2/day-2.py
@@ -34,4 +34,4 @@ print(type(True ))
 print(int("123") + int("456"))
 
 # Task - Make this line of code run without errors
-print("Number of letters in your name: " + (len(input("Enter your name"))))
+print("Number of letters in your name: " + str(len(input("Enter your name"))))
