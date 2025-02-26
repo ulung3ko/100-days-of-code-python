89a36ec408af846c713e781f4c4efe551225db2b
ulung3ko <akunke2eko@gmail.com>
2025-02-26 11:12:04 +0700

    Day-2 : String and Integers

--- day-2/day-2.py
+++ day-2/day-2.py
@@ -1,3 +1,9 @@
 # Subscripting
 # Display last character with negative number indexing
 print("Hello"[-1])
+
+# String
+print("123" + "345") # Concatenation
+
+# Integer = Whole number
+print(123 + 345)
