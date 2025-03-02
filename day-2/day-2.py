50922b7acaea13e23e14bb373d89c730dfdc7f6c
ulung3ko <akunke2eko@gmail.com>
2025-03-02 11:23:11 +0700

    Day-2 : f-strings concept

--- day-2/day-2.py
+++ day-2/day-2.py
@@ -89,3 +89,10 @@ print(score)
 score = 0
 score -= 1
 print(score)
+
+# f-strings
+score = 0
+height = 1.8
+is_winning = True
+
+print(f"Your score is = {score}, your height is {height}. You are winning is {is_winning}.")
