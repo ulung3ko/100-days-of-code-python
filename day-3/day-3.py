cdb93d2604e69f353b8fd3c3c96aa0f9517bc703
ulung3ko <akunke2eko@gmail.com>
2025-03-03 12:11:24 +0700

    day-3 : if/else statement

--- day-3/day-3.py
+++ day-3/day-3.py
@@ -2,7 +2,7 @@ print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
 
 # If Else (Conditional Statement)
-if height > 120:
+if height >= 120: # Greater than or equal to comparison operator
     print("You can ride the rollercoaster.")
 else:
     print("Sorry you have to grow taller before you can ride.")
