297c2da02f328327052fedfed094bf10a8f88431
ulung3ko <akunke2eko@gmail.com>
2025-03-03 10:58:38 +0700

    Day-2 project : Change total_bill's data type.

--- day-2/tip-calculator.py
+++ day-2/tip-calculator.py
@@ -1,14 +1,10 @@
 # Tip Calculator Project
-# This Program
+# This Program will calculate the amount of money each person has to pay.
 print("Welcome to the tip calculator!")
-total_bill = input("What was the total bill? $")
-tip = input("How much tip would you like to give? 10, 12, or 15? ")
-peoples = input("How many people to split the bill? ")
-
-total_bill = int(total_bill)
-tip = int(tip)
-peoples = int(peoples)
+total_bill = float(input("What was the total bill? $")) #Float is more accurate for bill
+tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
+peoples = int(input("How many people to split the bill? "))
 
 bill_with_tip = total_bill + (total_bill * (tip /100))
 result = bill_with_tip / peoples
-print(f"Each person should pay: {round(result, 2)}")
+print(f"Each person should pay: ${round(result, 2)}")
