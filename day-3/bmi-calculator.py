fbffdbc61efc136f32c139bebad28ea321acc0f6
ulung3ko <akunke2eko@gmail.com>
2025-03-04 11:12:28 +0700

    day-3 : coding exercise - bmi-calculator with interpretations.

---
+++ day-3/bmi-calculator.py
@@ -0,0 +1,13 @@
+weight = 85
+height = 1.85
+
+bmi = weight / (height ** 2)
+
+# 🚨 Do not modify the values above
+# Write your code below 👇
+if bmi < 18.5:
+    print("undrweight")
+elif bmi < 25:
+    print("normal weight")
+else:
+    print("overweight")
