487a501a3f275dc7a7841f4a662de49729507be5
ulung3ko <akunke2eko@gmail.com>
2025-03-04 11:07:39 +0700

    day-3 : nested if/else and elif statement.

--- day-3/day-3.py
+++ day-3/day-3.py
@@ -4,5 +4,12 @@ height = int(input("What is your height in cm? "))
 # If Else (Conditional Statement)
 if height >= 120: # Greater than or equal to comparison operator
     print("You can ride the rollercoaster.")
+    age = int(input("What is your age? "))
+    if age <= 12:
+        print("Please pay %5.")
+    elif age <= 18:
+        print("Please pay $7.")
+    else:
+        print("Please pay $12.")
 else:
     print("Sorry you have to grow taller before you can ride.")
