47df68030b276534d8d4b5fdb3535177c3009065
ulung3ko <akunke2eko@gmail.com>
2025-03-05 10:56:27 +0700

    day-3 : rollercoaster ticketing with multiple if statement.

--- day-3/day-3.py
+++ day-3/day-3.py
@@ -1,15 +1,27 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
+bill = 0
 
 # If Else (Conditional Statement)
 if height >= 120: # Greater than or equal to comparison operator
     print("You can ride the rollercoaster.")
     age = int(input("What is your age? "))
     if age <= 12:
-        print("Please pay %5.")
+        bill = 5
+        print("Child tickets are $5.")
     elif age <= 18:
-        print("Please pay $7.")
+        bill = 7
+        print("Youth tickets are $7.")
     else:
-        print("Please pay $12.")
+        bill = 12
+        print("Adult tickets are $12.")
+
+    wants_photo = input("Do you want to have a photo take? Type y for Yes and n for No.")
+    if wants_photo == "y":
+        # Add $3 to their bill
+        bill += 3
+
+    print(f"Your final bill is ${bill}.")
+
 else:
     print("Sorry you have to grow taller before you can ride.")
