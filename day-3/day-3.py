c72095ebd53706e9f302151ac3e0973ae600c89c
ulung3ko <akunke2eko@gmail.com>
2025-03-08 16:48:10 +0700

    day-3 : rollercoaster ticketing with logical operator.

--- day-3/day-3.py
+++ day-3/day-3.py
@@ -12,11 +12,13 @@ if height >= 120: # Greater than or equal to comparison operator
     elif age <= 18:
         bill = 7
         print("Youth tickets are $7.")
+    elif age >= 45 and age <= 55:
+        print("Everything is going to be ok. Have a free ride on us!")
     else:
         bill = 12
         print("Adult tickets are $12.")
 
-    wants_photo = input("Do you want to have a photo take? Type y for Yes and n for No.")
+    wants_photo = input("Do you want to have a photo take? Type y for Yes and n for No. ")
     if wants_photo == "y":
         # Add $3 to their bill
         bill += 3
