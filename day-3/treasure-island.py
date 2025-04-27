c9e1cd223e9f8fe0a45dc9f1bba1889a6c500768
ulung3ko <akunke2eko@gmail.com>
2025-04-27 19:51:27 +0700

    day-3 project : treasure island.

---
+++ day-3/treasure-island.py
@@ -0,0 +1,43 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/[TomekK]
+*******************************************************************************
+Welcome to the Treasure Island.
+Your mission is find the treasure.
+''')
+path_choice= input("You find yourself in an ancient forest."
+                    " Two narrow paths stretch out before you."
+                    " Do you go 'left' into the dark woods, or 'right' toward the misty hills?").lower()
+
+if path_choice == "right":
+    broken_bridge = input("After walking for hours, you reach a sparkling river blocking your way."
+                          " A broken bridge lies nearby."
+                          " Will you 'wait' to fix the bridge, or 'swim' across the rushing water?").lower()
+    if broken_bridge == "wait":
+        choose_door = input("Crossing safely, you find an ancient tower with three heavy doors:"
+                            " one golden, one silver, and one obsidian black. Which one do you open?").lower()
+        if choose_door == "golden":
+            print("You open the golden door and find a chamber glittering with treasures. You Win!")
+        else:
+            print("You trigger a deadly trap. Game Over.")
+    else:
+        print("The current is too strong! You are swept away downstream. Game Over.")
+else:
+    print("You wander into the mist and lose your way forever. Game Over.")
